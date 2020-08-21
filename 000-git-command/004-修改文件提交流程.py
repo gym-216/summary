c8b860331fb8163b5b27f37d9b2e02769ddda63a
guoyamin <15227812756@163.com>
2020-08-21 17:47:49 +0800

    git-command 修改文件提交流程

---
+++ 000-git-command/004-修改文件提交流程.py
@@ -0,0 +1,33 @@
+# 修改文件提交流程
+
+# git status
+# git diff 修改文件名
+# git add .  / git add 具体文件名
+# git commit -m "描述"
+# git pull origin master
+# git push -u  origin master
+# git log
+
+# 解释
+# git status 
+# 查看项目当前状态
+# git diff
+# 查看修改文件区别
+# git add . 
+# 跟踪文件 添加到暂存区
+# git commit
+# 提交到本地仓库
+# git pull origin 远程分支
+# 拉分支代码
+# git push -u origin 远程分支
+# 推送到远程分支
+# git log
+# 查看提交记录
+
+# 补充
+# git log
+# 查看所有提交记录
+# git log --oneline
+# 查看简写的提交记录
+# git log --author = "author" 
+# 查找某个作者所有提交记录
